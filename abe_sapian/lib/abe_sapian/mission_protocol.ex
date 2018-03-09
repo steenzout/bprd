b2293bd6961ba10ef9caf6640dc484a68f2a7b24
joshua.richardson <jrichocean@gmail.com>
2018-03-09 16:56:28 -0700

    ADDS CaptBenDaimio :check_in

--- abe_sapian/lib/abe_sapian/mission_protocol.ex
+++ abe_sapian/lib/abe_sapian/mission_protocol.ex
@@ -1,5 +1,10 @@
 defmodule AbeSapian.MissionProtocol do
    def check_in_with_captain() do
-
+      case Node.ping(:capt@localhost) do
+         :pong ->
+            :rpc.call(:capt@localhost, CaptBenDaimio, :check_in, [Node.self()])
+         _other ->
+            check_in_with_captain()
+      end
    end
 end
--- capt_ben_daimio/lib/capt_ben_daimio.ex
+++ capt_ben_daimio/lib/capt_ben_daimio.ex
@@ -3,9 +3,14 @@ defmodule CaptBenDaimio do
   Documentation for CaptBenDaimio.
   """
 
-    def checking_in(:true, node_name) do
-        get_agent = fn node_name -> <<agent, "@HollowEarth">> = Atom.to_string(node_name) end
-        "Thank you #{get_agent.(node_name)}."
+    @agent_call_signs ["abe", "liz", "hel", "joh", "rog"]
+
+    def check_in(node_name) do
+        <<agent::bytes-size(3), _::binary>> = Atom.to_string(node_name)
+        case agent in @agent_call_signs do
+            true -> "Thank You, Agent."
+            _else -> "WHO IS THIS AND HOW DID YOU GET THIS FREQUENCY?!"
+        end
     end
 
 end
