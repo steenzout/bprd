3cfe09f0e5830adf3ad1272a518aca67df3e47eb
jrichocean <jrichocean@gamil.com>
2018-03-22 09:28:10 -0600

    stash

--- abe_sapien/lib/abe_sapien/mission_protocol.ex
+++ abe_sapien/lib/abe_sapien/mission_protocol.ex
@@ -1,8 +1,8 @@
 defmodule AbeSapien.MissionProtocol do
    def check_in_with_captain() do
-      case Node.ping(:capt@localhost) do
+      case Node.ping(:capt@jrichocean) do
          :pong ->
-            :rpc.call(:capt@localhost, CaptBenDaimio, :check_in, [Node.self()])
+            :rpc.call(:capt@jrichocean, CaptBenDaimio, :check_in, [Node.self()])
          _other ->
             check_in_with_captain()
       end
